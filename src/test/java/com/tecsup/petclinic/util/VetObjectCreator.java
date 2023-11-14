f76613eb0a008c02079e3e4ddabaa3f7128e7631
Jhonchuri11 <jhon.churivanti@tecsup.edu.pe>
2023-11-14 11:54:05 -0500

    Borrando class Vet getVet()

--- src/test/java/com/tecsup/petclinic/util/VetObjectCreator.java
+++ src/test/java/com/tecsup/petclinic/util/VetObjectCreator.java
@@ -3,8 +3,7 @@ package com.tecsup.petclinic.util;
 import com.tecsup.petclinic.entities.Pet;
 
 public class VetObjectCreator {
-
-    public static class Vet getVet() {}
+    
     public static Pet getPet() {
         return new Pet(1,"Leo",1,1, null);
     }
