77fee9191dee87d0d9000d8484ea4a348d7cb6b2
Jhonchuri11 <jhon.churivanti@tecsup.edu.pe>
2023-11-13 19:53:23 -0500

    Subiendo la creación dentro del paquete Entities la class Vet

---
+++ src/main/java/com/tecsup/petclinic/entities/Vet.java
@@ -0,0 +1,29 @@
+package com.tecsup.petclinic.entities;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "vets")
+@Data
+public class Vet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String first_name;
+    private String last_name;
+
+    public Vet() {
+
+    }
+    public Vet(Integer id, String first_name, String last_name) {
+        super();
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+}
