a268493d032c9dc6a9e11f60f8ec81ec7565152f
Jhonchuri11 <jhon.churivanti@tecsup.edu.pe>
2023-11-13 20:19:26 -0500

    Creando en el paquete exception, la class betNotFoundException

---
+++ src/main/java/com/tecsup/petclinic/exception/vetNotFoundException.java
@@ -0,0 +1,12 @@
+package com.tecsup.petclinic.exception;
+
+public class vetNotFoundException extends Exception {
+
+    private static final long SerialVersionUID = 1L;
+
+    public vetNotFoundException(String message) {
+        super(message);
+
+        // TODO Auto-generated constructor stub
+    }
+}
